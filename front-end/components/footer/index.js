3db5f66095f055ba62211c27626fe37a4c3155ed
Andrew Vy <andrew@andrewvy.com>
2019-01-31 19:38:33 -0800

    :sparkles: Add footer component.

--- front-end/components/footer/index.js
+++ front-end/components/footer/index.js
@@ -1,16 +1,67 @@
+import React from 'react'
 import Link from 'next/link'
 
+import WhiteLogo from '../../svgs/brand/standalone_white_icon.svg'
+
+import DiscordLogo from '../../svgs/social/discord.svg'
+import TwitterLogo from '../../svgs/social/twitter.svg'
+
+import './index.css'
+
 const Footer = () => (
-  <>
-    <footer>
-      <Link href='/'><a>Home</a></Link>
-      <a href='https://twitter.com/dojinlist' target='_blank'>Twitter</a>
-      <a href='mailto:team@dojinlist.co'>Contact us</a>
-      <Link href='/terms'><a>Terms</a></Link>
-      <a href='/privacy'>Privacy</a>
-      <a href='/cookie-policy'>Cookie Policy</a>
-    </footer>
-  </>
+  <footer className='djn-footer'>
+    <div className='upper'>
+      <div className='left'>
+        <p className='description'>
+          Friendly music marketplace for self-published creators. An eccletic beat on
+          the underworld of doujin music, electronic culture, and the ins-and-outs of
+          running an independent music community.
+        </p>
+      </div>
+      <div className='right'>
+        <div className='col-1'>
+          <div className='link'>
+            <Link href='/'><a>Discover</a></Link>
+          </div>
+          <div className='link'>
+            <Link href='/'><a>Albums</a></Link>
+          </div>
+          <div className='link'>
+            <Link href='/'><a>Artists</a></Link>
+          </div>
+          <div className='link'>
+            <Link href='/'><a>Sign Up</a></Link>
+          </div>
+        </div>
+
+        <div className='col-2'>
+          <div className='link'>
+            <Link href='/terms'><a>Terms</a></Link>
+          </div>
+          <div className='link'>
+            <a href='/privacy'>Privacy</a>
+          </div>
+          <div className='link'>
+            <a href='/cookie-policy'>Cookie Policy</a>
+          </div>
+        </div>
+      </div>
+
+      <div className='logo'>
+        <WhiteLogo />
+      </div>
+    </div>
+
+    <div className='lower'>
+      <div className='social'>
+        <a href='https://twitter.com/dojinlist' target='_blank'><TwitterLogo /></a>
+        <a href='https://discord.gg/HT4d9nR' target='_blank'><DiscordLogo /></a>
+      </div>
+      <p>
+        Copyright ©  2019. Dojinlist LLC. All Rights Reserved.
+      </p>
+    </div>
+  </footer>
 )
 
 export default Footer
