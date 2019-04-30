6213975e1ff67501d251489374a430ca542186bd
Andrew Vy <andrew@andrewvy.com>
2019-04-30 16:23:21 -0700

    :sparkles: Add support for uploading Storefront banner images

--- api/lib/dojinlist_web/mutations/storefront.ex
+++ api/lib/dojinlist_web/mutations/storefront.ex
@@ -20,6 +20,17 @@ defmodule DojinlistWeb.Mutations.Storefront do
 
       resolve(&upload_avatar/2)
     end
+
+    field :upload_banner, type: :storefront do
+      arg(:banner, non_null(:upload))
+      arg(:storefront_id, non_null(:id))
+
+      middleware(DojinlistWeb.Middlewares.Authorization)
+      middleware(Absinthe.Relay.Node.ParseIDs, storefront_id: :storefront)
+      middleware(DojinlistWeb.Middlewares.StorefrontAuthorized, storefront_id: :storefront)
+
+      resolve(&upload_banner/2)
+    end
   end
 
   def create_storefront(%{storefront: storefront_attrs}, %{context: %{current_user: user}}) do
@@ -49,5 +60,20 @@ defmodule DojinlistWeb.Mutations.Storefront do
   def handle_avatar(avatar) do
     Dojinlist.Uploaders.rewrite_upload(avatar)
     |> Dojinlist.AvatarAttachment.store()
+  end
+
+  def upload_banner(attrs, %{context: %{current_user: _, storefront: storefront}}) do
+    with {:ok, banner} <- handle_banner(attrs[:banner]),
+         attrs = %{banner_image: banner},
+         {:ok, storefront} <- Dojinlist.Storefront.update_storefront(storefront, attrs) do
+      {:ok, storefront}
+    else
+      {:error, _} = error -> error
+    end
+  end
+
+  def handle_banner(banner) do
+    Dojinlist.Uploaders.rewrite_upload(banner)
+    |> Dojinlist.BannerAttachment.store()
   end
 end
