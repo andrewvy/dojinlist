ed560c480d082103f3230b6bc5e939afc2b8f87c
Andrew Vy <andrew@andrewvy.com>
2019-05-02 20:58:34 -0700

    :sparkles: Initial storefront updates

--- front-end/components/album_thumbnail/index.js
+++ front-end/components/album_thumbnail/index.js
@@ -5,7 +5,12 @@ import './index.css'
 const AlbumThumbnail = ({ album }) => {
   return (
     <div className='djn-albumThumbnail'>
-      <img className='thumbnail' src={album.coverArtUrl} />
+      <img
+        className='thumbnail'
+        src={album.coverArtUrl}
+        width={180}
+        height={180}
+      />
       <div className='album-meta'>
         <div className='album-name'>{album.title}</div>
         <div className='album-artist'>{album.artistName}</div>
--- front-end/pages/storefront.js
+++ front-end/pages/storefront.js
@@ -2,12 +2,12 @@ import React, { PureComponent } from 'react'
 import { Query } from 'react-apollo'
 import ErrorPage from 'next/error'
 
-import withNavigation from '../components/navigation'
-
 import FetchStorefrontQuery from '../queries/storefront/storefront.js'
 import FetchAlbumsByStorefrontId from '../queries/albums/by_storefront_id.js'
 
+import withNavigation from '../components/navigation'
 import AlbumThumbnailGrid from '../components/album_thumbnail_grid'
+import Pill from '../components/pill'
 
 import Page from '../layouts/main.js'
 
@@ -25,54 +25,59 @@ class HomePage extends PureComponent {
 
     return (
       <Page>
-        <div className='container'>
-          <Query
-            query={FetchStorefrontQuery}
-            variables={{ slug: storefront_slug }}
-          >
-            {({ data, loading, error }) => (
-              <div>
-                {!loading && data && (
-                  <>
-                    <div className='djn-storefrontHeader'>
-                      <div className='djn-storefrontBanner'>
-                        <img src={data.storefront.bannerImage} />
-                      </div>
-                      <div className='djn-storefrontAvatar'>
-                        <div className='avatar-container'>
-                          <img src={data.storefront.avatarImage} />
+        <div className='djn-storefrontPageContainer'>
+          <div className='djn-storefrontPage container'>
+            <Query
+              query={FetchStorefrontQuery}
+              variables={{ slug: storefront_slug }}
+            >
+              {({ data, loading, error }) => (
+                <div>
+                  {!loading && data && (
+                    <>
+                      <div className='djn-storefrontHeader'>
+                        <div className='djn-storefrontBanner'>
+                          <img src={data.storefront.bannerImage} />
                         </div>
-                        <div className='display-name'>
-                          {data.storefront.displayName}
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className='djn-storefrontPage'>
-                      <Query
-                        query={FetchAlbumsByStorefrontId}
-                        variables={{
-                          storefrontId: data.storefront.id,
-                          first: 10
-                        }}
-                      >
-                        {({ data, loading, error }) => (
-                          <div className='albums'>
-                            <AlbumThumbnailGrid
-                              albums={data.albums.edges.map(transformAlbums)}
-                              storefront_slug={storefront_slug}
-                            />
+                        <div className='djn-storefrontAvatar'>
+                          <div className='avatar-container'>
+                            <img src={data.storefront.avatarImage} />
                           </div>
-                        )}
-                      </Query>
-                    </div>
-                  </>
-                )}
+                          <div className='display-name'>
+                            {data.storefront.displayName}
+                          </div>
+                        </div>
+                      </div>
 
-                {error && <ErrorPage statusCode={404} />}
-              </div>
-            )}
-          </Query>
+                      <div className='content'>
+                        <div className='storefront-description'>
+                          {data.storefront.description}
+                        </div>
+                        <Query
+                          query={FetchAlbumsByStorefrontId}
+                          variables={{
+                            storefrontId: data.storefront.id,
+                            first: 10
+                          }}
+                        >
+                          {({ data, loading, error }) => (
+                            <div className='albums'>
+                              <AlbumThumbnailGrid
+                                albums={data.albums.edges.map(transformAlbums)}
+                                storefront_slug={storefront_slug}
+                              />
+                            </div>
+                          )}
+                        </Query>
+                      </div>
+                    </>
+                  )}
+
+                  {error && <ErrorPage statusCode={404} />}
+                </div>
+              )}
+            </Query>
+          </div>
         </div>
       </Page>
     )
