44221e01a72887db58c307d8a7ba1c304f0ca0e8
Andrew Vy <andrew@andrewvy.com>
2019-05-23 17:41:51 -0700

    :zap: Track file drag handle + styles

--- front-end/components/album_creator/track_list.js
+++ front-end/components/album_creator/track_list.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 import {
   sortableContainer,
   sortableElement,
@@ -6,7 +8,9 @@ import {
 
 import classNames from 'classnames'
 
-const DragHandle = sortableHandle(() => <div className='drag-handle'>::</div>)
+import IconDrag from '../../svgs/icons/icon-drag.svg'
+
+const DragHandle = sortableHandle(() => <div className='drag-handle'><IconDrag fill='#FF6D6C'/></div>)
 
 const SortableItem = sortableElement(({ value, index, onChange }) => (
   <li className='djn-file'>
